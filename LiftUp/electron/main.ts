99fbb4faab17442f91e885a9c7f401262591195e
lntreter <111744121+lntreter@users.noreply.github.com>
2024-02-28 14:22:27 +0300

    Bugs fixed

--- LiftUp/electron/main.ts
+++ LiftUp/electron/main.ts
@@ -62,16 +62,102 @@ app.on('activate', () => {
   }
 })
 
-ipcMain.handle('read-excel', async (event, arg) => {
+// ipcMain.handle('read-excel', async (event, arg) => {
+//   try {
+//     if(fs.existsSync(arg)) {
+//       console.log('arg: ', arg);
+//       const fileBuffer = fs.readFileSync(arg);
+//       console.log('fileBuffer: ', fileBuffer);
+//       const data = XLSX.read(fileBuffer, {type: 'buffer'});
+//       return data;
+//     } else {
+//       throw new Error(`File does not exist: ${arg}`);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     throw err;
+//   }
+// });
+
+
+ipcMain.handle('read-excel', async (_event, arg) => {
   try {
     if(fs.existsSync(arg)) {
       console.log('arg: ', arg);
-      const data = XLSX.readFile(arg);
+      const fileBuffer = fs.readFileSync(arg);
+      console.log('fileBuffer: ', fileBuffer);
+      const data = XLSX.read(fileBuffer, {type: 'buffer'});
+      const workbook = data
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+
+      // JSON'a çevirme işlemi başlar. İlk satırı atlamak için range: 1 eklenir.
+      let jsonData = XLSX.utils.sheet_to_json(worksheet, { header:1, range: 2 });
+
+
+      console.log('jsonData: ', jsonData);
+
+      // JSON verilerini istenen yapıya dönüştür
+      jsonData = jsonData.map((row:any) => {
+        // Yeni bir obje oluştur
+        let newRow:any = {};
+        // Başlıkları manuel olarak eşleştir
+        newRow[''] = {
+          'Number': row[0],
+        };
+        newRow['Signal'] = {
+          'Name': row[1],
+          'TYPE': row[2],
+          'CATEGORY': row[3],
+          'CURRENT(Max)': row[4]
+        };
+        newRow['CABLE'] = {
+          'TYPE': row[5],
+          'AWG': row[6]
+        };
+        newRow['Source'] = {
+          'ATA CHAPTER': row[7],
+          'PIN NAME': row[8],
+          'LOCATION': row[9],
+          'LRU': row[10],
+          'RD NUMBER': row[11],
+          'Connector': row[12],
+          'Pin No': row[13]
+        };
+        newRow['Destination'] = {
+          'ATA CHAPTER': row[14],
+          'PIN NAME': row[15],
+          'LOCATION': row[16],
+          'LRU': row[17],
+          'RD NUMBER': row[18],
+          'Connector': row[19],
+          'Pin No': row[20]
+        };
+          
+        // Eğer daha fazla sütun varsa, onları da burada ekleyebilirsiniz.
+
+        return newRow;
+      });
+
+      // JSON verisini bir dosyaya yaz
+
+      const outputFilePath = "./src/components/Output.json"
+
+
+      fs.writeFile(outputFilePath, JSON.stringify(jsonData, null, 2), (err:any) => {
+        if (err) {
+          console.error('JSON dosyası yazılırken bir hata oluştu:', err);
+          return;
+        }
+        console.log(`${outputFilePath} üzerine JSON dosyası başarıyla yazıldı.`);
+      });
+
       return data;
     } else {
       throw new Error(`File does not exist: ${arg}`);
     }
   } catch (err) {
+    console.error(err);
     throw err;
   }
 });
--- LiftUp/src/App.tsx
+++ LiftUp/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import TLogo from './assets/logo.png'
 import LULogo from './assets/liftup2.png'
-import * as XLSX from 'xlsx'
 import './App.css'
 
 
@@ -14,71 +13,12 @@ function App() {
   }
   , [fileDirectory])
   
-  const Convert = async (inputFilePath:string , outputFilePath:string) => {
+  const Convert = async (inputFilePath:string) => {
     try {
       console.log('inputFilePath: ', inputFilePath)
-      const fileData = await window.ipcRenderer.invoke('read-excel', inputFilePath);
-      const workbook = XLSX.readFile(fileData);
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      
-      // JSON'a çevirme işlemi başlar. İlk satırı atlamak için range: 1 eklenir.
-      let jsonData = XLSX.utils.sheet_to_json(worksheet, { header:1, range: 2 });
+      await window.ipcRenderer.invoke('read-excel', inputFilePath);
 
-      console.log('jsonData: ', jsonData);
-      
-      // JSON verilerini istenen yapıya dönüştür
-      jsonData = jsonData.map((row:any) => {
-        // Yeni bir obje oluştur
-        let newRow:any = {};
-        // Başlıkları manuel olarak eşleştir
-        newRow[''] = {
-          'Number': row[0],
-        };
-        newRow['Signal'] = {
-          'Name': row[1],
-          'TYPE': row[2],
-          'CATEGORY': row[3],
-          'CURRENT(Max)': row[4]
-        };
-        newRow['CABLE'] = {
-          'TYPE': row[5],
-          'AWG': row[6]
-        };
-        newRow['Source'] = {
-          'ATA CHAPTER': row[7],
-          'PIN NAME': row[8],
-          'LOCATION': row[9],
-          'LRU': row[10],
-          'RD NUMBER': row[11],
-          'Connector': row[12],
-          'Pin No': row[13]
-        };
-        newRow['Destination'] = {
-          'ATA CHAPTER': row[14],
-          'PIN NAME': row[15],
-          'LOCATION': row[16],
-          'LRU': row[17],
-          'RD NUMBER': row[18],
-          'Connector': row[19],
-          'Pin No': row[20]
-        };
-          
-        // Eğer daha fazla sütun varsa, onları da burada ekleyebilirsiniz.
-  
-        return newRow;
-      });
-  
-      // JSON verisini bir dosyaya yaz
-      
-
-      // fs.writeFile(outputFilePath, JSON.stringify(jsonData, null, 2), (err:any) => {
-      //   if (err) {
-      //     console.error('JSON dosyası yazılırken bir hata oluştu:', err);
-      //     return;
-      //   }
-      //   console.log(`${outputFilePath} üzerine JSON dosyası başarıyla yazıldı.`);
-      // });
+      console.log('JSON dosyası oluşturuldu.')
       
     } catch (error) {
       console.error('Excel dosyası işlenirken bir hata oluştu:', error);
@@ -111,7 +51,7 @@ function App() {
           <div className='devreB'>
             {fileExtension == "xlsx" ? <button onClick={() => {
 
-              Convert(fileDirectory, './components/Devreler.json')
+              Convert(fileDirectory)
               console.log(fileDirectory)
 
             }} className='devreB'> Devreyi çizdir! </button> : 
